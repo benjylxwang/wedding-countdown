3ea8895af41d9086d4fc2bb8d31bcc5153957640
Benjamin Wang <benjylxwang64@gmail.com>
2021-05-26 15:52:42 +0100

    Hopefully a fix for mobile image quality

--- docs/sw.js
+++ docs/sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6d427f1da3ba65ada4e8.js"
+    "url": "webpack-runtime-a0943a10d9c737136a80.js"
   },
   {
     "url": "framework-9f754d8b33503a24e84f.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "53219ef38b1ba40e0a711fad550e5da6"
+    "revision": "46082a158af83f049d7d13c638fa0897"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "354e0f4c7fd0f05780c90b1728265175"
+    "revision": "242b824d21381fb7050147b7f30f85f9"
   },
   {
     "url": "polyfill-140e3b63fc9834dd4e0f.js"
